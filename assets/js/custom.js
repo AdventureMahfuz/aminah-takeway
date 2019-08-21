8ed6e13b0781f14745d7e1707c984bf56718fd07
AdventureMahfuz <adventure.mahfuz@gmail.com>
2019-08-21 20:36:49 +0600

    added reservation page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,6 +28,33 @@ $(document).ready(function(){
     });
 
 
+    //time slider
+    $('.all-times').owlCarousel({
+        loop: false,
+        margin: 0,
+        autoplay: false,
+        responsiveClass: true,
+        nav: true,
+        dots: false,
+        mouseDrag: true,
+        touchDrag: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 1,
+                nav: true
+            }
+        }
+    });
+
+
     //hero slider
     $('.hero-slider').owlCarousel({
         animateIn: 'fadeIn',
